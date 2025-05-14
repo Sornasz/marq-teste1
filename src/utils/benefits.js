599494f1fc252c233bbe98eda881bb2e61b0a802
Arthur Sornas <sornasarthur93@gmail.com>
2025-05-14 20:22:18 -0300

    Implementando a lógica com a função classifyBeneifit.

---
+++ src/utils/benefits.js
@@ -0,0 +1,13 @@
+export function classifyBenefit (description) {
+    const = categories {
+        Alimentação: ["restaurante", "supermercado", "padaria"],
+        Saúde: ["farmácia", "hospital", "clínica"],
+        Transporte: ["uber", "taxi", "ônibus"],
+        Cultura: ["cinema", "teatro", "museu"],
+        Educação: ["cursos", "universidade", "biblioteca"],        
+    };
+
+//Implementando a lógica
+
+return "A Implementar";
+}
